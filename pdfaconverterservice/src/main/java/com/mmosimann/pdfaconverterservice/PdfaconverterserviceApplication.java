7eca51980149d7d9129c40b09de50d0f0f988ee7
samatthias <samatthias@m-mosimann.com>
2023-01-31 13:04:27 +0100

    init service

---
+++ pdfaconverterservice/src/main/java/com/mmosimann/pdfaconverterservice/PdfaconverterserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mmosimann.pdfaconverterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfaconverterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfaconverterserviceApplication.class, args);
+	}
+
+}
